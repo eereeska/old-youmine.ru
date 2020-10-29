78c29aeebfe8ff15ea9f0e2acb1cb162f8a8048d
eereeska <eereeskee@gmail.com>
2020-10-29 22:43:47 +0100

    вч

--- app/Http/Controllers/Server/AuthController.php
+++ app/Http/Controllers/Server/AuthController.php
@@ -10,21 +10,20 @@ class AuthController extends Controller
 {
     public function preLogin(Request $r)
     {
-        $user = User::where('name', $r->name)->first();
+        $user = User::where('name', $r->name)->orWhere(function($query) use ($r) {
+            $query->where('ip', $r->ip)->where('name', null);
+        })->first();
 
         if (!$user) {
-            $user_without_name = User::where('ip', $r->ip)->where('name', null)->first();
+            return response()->json([
+                'allow' => false,
+                'message' => 'хз'
+            ]);
+        }
 
-            if (!$user_without_name) {
-                return response()->json([
-                    'allow' => false,
-                    'message' => 'хз'
-                ]);
-            } else {
-                $user = $user_without_name;
-                $user->name = $r->name;
-                $user->save();
-            }
+        if (is_null($user->name)) {
+            $user->name = $r->name;
+            $user->save();
         }
 
         if ((!$user->admin and !$user->moderator) and $user->sub_expire_at->lte(now())) {
