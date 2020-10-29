2d0f31a569d9068262a13b791685758cd3db5ae3
eereeska <eereeskee@gmail.com>
2020-10-29 22:53:09 +0100

    sub column

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,6 +23,7 @@ class CreateUsersTable extends Migration
             $table->string('ip')->nullable();
             $table->string('reg_country')->nullable();
             $table->string('country')->nullable();
+            $table->timestamp('sub_expire_at')->useCurrent();
             $table->rememberToken();
             $table->timestamps();
         });
