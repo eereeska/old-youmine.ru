b161faf191515e46209126f11cbcaabc1fef0abf
eereeska <eereeskee@gmail.com>
2020-11-16 19:59:18 +0300

    auth ip resposne fix

--- app/Http/Controllers/Server/AuthController.php
+++ app/Http/Controllers/Server/AuthController.php
@@ -40,7 +40,7 @@ class AuthController extends Controller
         return response()->json([
             'allow' => true,
             'exists' => !!$user,
-            'ip' => $r->ip,
+            'ip' => $user ? $user->ip : null,
             'skin' => [
                 'texture' => $skin->texture,
                 'signature' => $skin->signature
